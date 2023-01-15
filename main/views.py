c0bc60454423a7d909399619829b2061bb3d3871
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-01-15 22:54:03 +0300

    second commit

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def redirect_from_root(request):
+    return redirect(to='/main/')
