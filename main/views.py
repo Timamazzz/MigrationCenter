772793d8bf89f6ceae2aa30a25603c196639ecf5
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-01-23 00:10:34 +0300

    register model in admin and upd main

--- main/views.py
+++ main/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
+from news.models import *
+from main.models import *
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'dist/index.html')
+    news = News.objects.all()
+    informations = Information.objects.all()
+    return render(request, 'dist/index.html', {'news': news, 'informations':informations})
 
 
 def redirect_from_root(request):
