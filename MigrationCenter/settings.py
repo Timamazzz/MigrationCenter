93f072cd601dd3db11412c3f8a95a0ec74fbfd1f
Timama <timforworking@mail.ru>
2023-03-01 02:28:58 +0300

    setting env 6

--- MigrationCenter/settings.py
+++ MigrationCenter/settings.py
@@ -129,10 +129,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static/",
-    'templates/'
-]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
