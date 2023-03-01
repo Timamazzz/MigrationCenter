68d8a2d35a376403a73bc384a298d36d16a4eef4
Timama <timforworking@mail.ru>
2023-03-01 05:16:12 +0300

    wsgi edit

--- MigrationCenter/wsgi.py
+++ MigrationCenter/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for MigrationCenter project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+sys.path.insert(0, '/var/www/u0545518/data/www/cppsta.ru/MigrationCenter')
+sys.path.insert(0, '/var/www/u0545518/data/www/cppsta.ru/MigrationCenter/MigrationCenter')
+sys.path.insert(1, '/var/www/u0545518/data/djangoenv/lib/python3.10.1/site-packages')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'MigrationCenter.settings'
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MigrationCenter.settings')
-
 application = get_wsgi_application()
