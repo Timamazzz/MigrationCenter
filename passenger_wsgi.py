9012912b4c86ccc6a34b26245c1d095d070539fd
Timama <timforworking@mail.ru>
2023-03-01 06:30:16 +0300

    wsgi edit

--- passenger_wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-sys.path.insert(0, '/var/www/u0545518/data/www/cppsta.ru/MigrationCenter')
-sys.path.insert(0, '/var/www/u0545518/data/www/cppsta.ru/MigrationCenter/MigrationCenter')
-sys.path.insert(1, '/var/www/u0545518/data/djangoenv/lib/python3.10.1/site-packages')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'MigrationCenter.settings'
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
