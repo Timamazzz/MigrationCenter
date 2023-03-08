d7099bd28b30c19a6222fb6f3e803a1b92c5581a
Timama <timforworking@mail.ru>
2023-03-08 16:26:19 +0300

    change env

--- MigrationCenter/urls.py
+++ MigrationCenter/urls.py
@@ -30,7 +30,4 @@ urlpatterns = [
     path('', views.redirect_from_root, name='redirect from root')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
 handler404 = 'main.views.handler404'
