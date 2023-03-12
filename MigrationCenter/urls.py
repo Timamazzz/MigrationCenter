cad712834d93eab9a8b5147f422bf4bb4f7b145b
Timama <timforworking@mail.ru>
2023-03-13 01:47:30 +0300

    fixing2

--- MigrationCenter/urls.py
+++ MigrationCenter/urls.py
@@ -19,7 +19,6 @@ import MigrationCenter.views
 from main import views
 from django.conf.urls.static import static
 from django.conf import settings
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include('main.urls')),
@@ -32,5 +31,5 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 handler404 = 'MigrationCenter.views.handler404'
