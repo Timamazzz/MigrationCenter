224cb736de6ac00550d4c01d127b654476b264ae
Timama <timforworking@mail.ru>
2023-03-12 03:02:17 +0300

    multiply add images in news

--- news/admin.py
+++ news/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(PostImage)
 
+# Register your models here.
+class PostImageAdmin(admin.StackedInline):
+    model = PostImage
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [PostImageAdmin]
+    model = Post
+
+
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    pass
